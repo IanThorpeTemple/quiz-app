b463d5723d3640bc637853952dd294599f59988b
Romulad <romualdnoualinon@gmail.com>
2024-04-30 16:13:53 +0100

    Front : Update endpoint

--- front-end/src/config.js
+++ front-end/src/config.js
@@ -1,5 +1,5 @@
 
 export const baseUrl = (
-    window.location.host.includes("localhost") ? 
-    "http://127.0.0.1:5000/api" : `https://quiz-geiw.onrender.com/api`
+    window.location.host.includes("localhost") ?
+    "http://127.0.0.1:5000/api" : `https://quiz-app-qmk4.onrender.com/api`
 )
