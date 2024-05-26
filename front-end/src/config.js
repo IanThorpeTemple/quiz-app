c251eb4c6de78988d80c953f21179df3f740df80
Romulad <romualdnoualinon@gmail.com>
2024-05-26 19:10:30 +0100

    playing with compose

--- front-end/src/config.js
+++ front-end/src/config.js
@@ -1,5 +1,5 @@
 
-export const baseUrl = "http://ip172-18-0-56-cp9niool2o9000b04r4g@direct.labs.play-with-docker.com/api"
+export const baseUrl = "http://ip172-18-0-45-cp9nlmqim2rg00f98ijg@direct.labs.play-with-docker.com/api"
 
 // (
 //     window.location.host.includes("localhost") ?
