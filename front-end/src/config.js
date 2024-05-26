530b2387c1e7931206a32825ee9c1dacb44e740e
Romulad <romualdnoualinon@gmail.com>
2024-05-26 19:36:31 +0100

    playing with compose

--- front-end/src/config.js
+++ front-end/src/config.js
@@ -1,7 +1,5 @@
 
-export const baseUrl = "http://ip172-18-0-45-cp9nlmqim2rg00f98ijg@direct.labs.play-with-docker.com/api"
-
-// (
-//     window.location.host.includes("localhost") ?
-//     "http://127.0.0.1:5000/api" : `https://quiz-app-qmk4.onrender.com/api`
-// )
+export const baseUrl = (
+    window.location.host.includes("localhost") ?
+    "http://127.0.0.1:5000/api" : `https://quiz-app-qmk4.onrender.com/api`
+)
