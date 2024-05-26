e15a27b18093df66c6af14f56a3e1c815319613e
Romulad <romualdnoualinon@gmail.com>
2024-05-26 18:22:28 +0100

    playing with compose

--- front-end/src/config.js
+++ front-end/src/config.js
@@ -1,5 +1,7 @@
 
-export const baseUrl = (
-    window.location.host.includes("localhost") ?
-    "http://127.0.0.1:5000/api" : `https://quiz-app-qmk4.onrender.com/api`
-)
+export const baseUrl = "http://quiz_app_backend:5000/api"
+
+// (
+//     window.location.host.includes("localhost") ?
+//     "http://127.0.0.1:5000/api" : `https://quiz-app-qmk4.onrender.com/api`
+// )
